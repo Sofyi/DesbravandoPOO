65169584c115f269fc4e739ba1219789fb932149
Sofia Lunkes <sofia.big@live.com>
2016-05-09 22:03:51 -0300

    começando a arrumar o carrinho com arraylist

--- src/br/com/sofialunkes/livraria/teste/CarrinhoDeCompras.java
+++ src/br/com/sofialunkes/livraria/teste/CarrinhoDeCompras.java
@@ -2,11 +2,13 @@ package br.com.sofialunkes.livraria.teste;
 
 import java.util.ArrayList;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 import br.com.sofialunkes.livraria.produtos.*;
 
 public class CarrinhoDeCompras {
 	private double total;
-	ArrayList<Livro> lista = new ArrayList<Livro>();
+	ArrayList<Produto> lista = new ArrayList<Produto>();
 	public static int cont;
 	
 	public double getTotal() {
@@ -17,6 +19,12 @@ public class CarrinhoDeCompras {
 	}
 	public void adiciona(Produto produto){
 		System.out.println("Adicionando\n"+produto);
+		lista.add(produto);
 		setTotal(produto.getValor());
+	}
+	public void getProdutos(){
+		for(Produto produto : lista){
+			System.out.println(produto.toString());
+		}
 	}
 }
